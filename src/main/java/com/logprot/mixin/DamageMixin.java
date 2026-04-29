47da254750a00fcf7b328ac324b0918669e5e648
someaddons <someaddons@github.com>
2026-04-29 20:24:18 +0200

    26.1

--- src/main/java/com/logprot/mixin/DamageMixin.java
+++ src/main/java/com/logprot/mixin/DamageMixin.java
@@ -5,9 +5,7 @@ import com.logprot.players.PlayerManager;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.portal.DimensionTransition;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ServerPlayer.class)
 public class DamageMixin
 {
-    @Inject(at = @At("HEAD"), method = "hurt", cancellable = true)
-    private void onDamage(final DamageSource source, final float amount, final CallbackInfoReturnable<Boolean> cir)
+    @Inject(at = @At("HEAD"), method = "hurtServer", cancellable = true)
+    private void onDamage(final ServerLevel level, final DamageSource source, final float damage, final CallbackInfoReturnable<Boolean> cir)
     {
         if (PlayerManager.getInstance().isPlayerImmune((Player) (Object) this, source))
         {
@@ -26,19 +24,8 @@ public class DamageMixin
         }
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;sendActivePlayerEffects(Lnet/minecraft/server/level/ServerPlayer;)V"), method = "changeDimension")
-    private void onChangeDim(
-      final DimensionTransition dimensionTransition, final CallbackInfoReturnable<Entity> cir)
-    {
-        if (Logprot.config.getCommonConfig().dimensionprotection)
-        {
-            PlayerManager.getInstance().onPlayerLogin((ServerPlayer) (Object) this);
-        }
-    }
-
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;teleport(DDDFF)V"), method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDFF)V")
-    private void onTP(
-      final ServerLevel serverLevel, final double d, final double e, final double f, final float g, final float h, final CallbackInfo ci)
+    @Inject(at = @At(value = "HEAD"), method = "triggerDimensionChangeTriggers")
+    private void onChangeDim(final ServerLevel oldLevel, final CallbackInfo ci)
     {
         if (Logprot.config.getCommonConfig().dimensionprotection)
         {
@@ -48,9 +35,9 @@ public class DamageMixin
 
     @Inject(at = @At(value = "RETURN"), method = "restoreFrom")
     private void onRespawn(
-      final ServerPlayer serverPlayer,
-      final boolean bl,
-      final CallbackInfo ci)
+        final ServerPlayer serverPlayer,
+        final boolean bl,
+        final CallbackInfo ci)
     {
         if (Logprot.config.getCommonConfig().dimensionprotection)
         {
